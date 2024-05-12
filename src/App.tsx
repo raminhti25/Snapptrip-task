64d751101af4ffd682c30b892b5b834e6ca56fe7
Ramin Heshmati <raminhti@gmail.com>
2024-05-12 15:30:55 +0330

    Move useFetchData hook to hooks folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import useFetchData from "./useFetchData";
+import useFetchData from "./hooks/useFetchData";
 
 function App() {
   const { loading, error, data, refetch } = useFetchData(
